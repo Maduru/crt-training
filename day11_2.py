f7d66ce1bb6bce7df1eaeeda970ad25e1644865e
Maduru <229y1a0575@ksrmce.ac.in>
2025-02-01 13:28:51 +0530

    Update day11_2.py

--- day11_2.py
+++ day11_2.py
@@ -1,47 +1,47 @@
-# import sys
-# from day11 import*
-# s=Queue()
-# while True:
-#     print(" select the following option")
-#     print("\t\t1.enqueue")
-#     print("\t\t2.dequeue")
-#     print("\t\t3.peek")
-#     print("\t\t4.search")     
-#     print("\t\t5.display")
-#     print("\t\t6.Exit")
-#     o= int(input("enter option:"))
-#     if o==1:
-#         ele= int(input(" enter data :"))
-#         s.enqueue(ele)
-#     elif o==2:
-#         ret=s.dequeue()
-#         if ret==-1:
-#             print("stack is empty")
-#         else:
-#             print("popped element",ret)
+import sys
+from day11 import*
+s=Queue()
+while True:
+    print(" select the following option")
+    print("\t\t1.enqueue")
+    print("\t\t2.dequeue")
+    print("\t\t3.peek")
+    print("\t\t4.search")     
+    print("\t\t5.display")
+    print("\t\t6.Exit")
+    o= int(input("enter option:"))
+    if o==1:
+        ele= int(input(" enter data :"))
+        s.enqueue(ele)
+    elif o==2:
+        ret=s.dequeue()
+        if ret==-1:
+            print("stack is empty")
+        else:
+            print("popped element",ret)
         
-#     elif o==3:
-#         ret=s.peek()
-#         if ret==-1:
-#             print("stack is empty")
-#         else:
-#             print("peek element",ret)
+    elif o==3:
+        ret=s.peek()
+        if ret==-1:
+            print("stack is empty")
+        else:
+            print("peek element",ret)
 
-#     elif o==4:
-#         ele= int(input(" enter element to search"))
-#         ret=s.search(ele)
-#         if ret==-1:
-#             print("stack is empty")
-#         elif ret==-2:
-#             print("not found")
-#         else:
-#             print("found at index:",ret)
-#     elif o==5:
-#         s.display()
-#     elif o==6:
-#         sys.exit()
-#     else:
-#         print("enter valid option")
+    elif o==4:
+        ele= int(input(" enter element to search"))
+        ret=s.search(ele)
+        if ret==-1:
+            print("stack is empty")
+        elif ret==-2:
+            print("not found")
+        else:
+            print("found at index:",ret)
+    elif o==5:
+        s.display()
+    elif o==6:
+        sys.exit()
+    else:
+        print("enter valid option")
   
 n=5
 for i in range(0,n):
