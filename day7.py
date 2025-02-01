9c50a9a4adad1e065fc0bb4ab7ab4e1c98ea62fb
Maduru <229y1a0575@ksrmce.ac.in>
2025-02-01 13:32:54 +0530

    Update day7.py

--- day7.py
+++ day7.py
@@ -1,29 +1,29 @@
-# # def sum(*args):
-# #     sum=0
-# #     for i in args:
-# #         sum+=i
-# #     return sum
-# # v=sum(1,2,3,4,4,7)
-# # print(v)
+def sum(*args):
+    sum=0
+    for i in args:
+        sum+=i
+    return sum
+v=sum(1,2,3,4,4,7)
+print(v)
  
 
-# class product:
-#     def user(self):
-#         self.name= input("enter product name:")
-#         self.price =int(input("product price:"))
-#     def display(self):
-#         print("product name:",self.name)
-#         print("product price:",self.price)
-# a= product()
-# b=product()
-# a.user()
-# b.user()
-# print("----------------")
-# print("product 1")
-# a.display()
-# print("----------------")
-# print("product 2")
-# b.display()
+class product:
+    def user(self):
+        self.name= input("enter product name:")
+        self.price =int(input("product price:"))
+    def display(self):
+        print("product name:",self.name)
+        print("product price:",self.price)
+a= product()
+b=product()
+a.user()
+b.user()
+print("----------------")
+print("product 1")
+a.display()
+print("----------------")
+print("product 2")
+b.display()
 
 
 from abc import ABC,abstractmethod
